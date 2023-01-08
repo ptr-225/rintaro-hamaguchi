92f20d6f18b2c030772d9290c41ec9b6b7e7f24f
ptr-225 <122195344+ptr-225@users.noreply.github.com>
2023-01-08 14:59:34 +0900

    Add files via upload

---
+++ WEB__portfolio/css/common.js
@@ -0,0 +1,12 @@
+$('.banner').slick({
+    autoplay: true,
+    autoplaySpeed:10,
+    speed:10,
+    dots: false,
+    arrows: false,
+    slidesToShow: 2,
+    infinite:true,
+    focusOnSelect: true,
+    speed : 100,
+    }
+);
