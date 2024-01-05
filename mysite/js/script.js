564f5e74527fe9dd8e89c05967b871cfe0a95da1
ptr-225 <122195344+ptr-225@users.noreply.github.com>
2024-01-05 14:30:26 +0900

    Add files via upload

---
+++ mysite/js/script.js
@@ -0,0 +1,21 @@
+// fadeアニメーションの調整
+
+$(window).on("load", function () {
+  $("body").removeClass("fade");
+});
+$(function () {
+  // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+  $('a:not([href^="#"]):not([target])').on("click", function (e) {
+    e.preventDefault(); // ナビゲートをキャンセル
+    url = $(this).attr("href"); // 遷移先のURLを取得
+    if (url !== "") {
+      $("body").addClass("fade"); // bodyに class="fadeout"を挿入
+      setTimeout(function () {
+        window.location = url; // 0.2秒後に取得したURLに遷移
+      }, 200);
+    }
+    return false;
+  });
+});
+
+// /fadeアニメーションの調整
