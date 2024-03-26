1905544a309c94733fc0d072204de4d20c341df6
ptr-225 <122195344+ptr-225@users.noreply.github.com>
2024-03-26 14:26:46 +0900

    Delete demo/1/1 directory

--- demo/1/1/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-      //ハンバーガーメニューをクリックするたびにactiveクラスの付与を切り替える
-      document.querySelector('.hamburger').addEventListener('click', function(){
-        this.classList.toggle('active');
-        document.querySelector('.slide-menu__section').classList.toggle('active');
-      })
-    })
