4a411b8e6bdce06f42235e96b803c20e0b353f3c
ptr-225 <122195344+ptr-225@users.noreply.github.com>
2024-03-28 16:14:26 +0900

    Add files via upload

--- demo/1/js/main.js
+++ demo/1/js/main.js
@@ -1,7 +1,23 @@
-window.addEventListener('DOMContentLoaded', function() {
-      //ハンバーガーメニューをクリックするたびにactiveクラスの付与を切り替える
-      document.querySelector('.hamburger').addEventListener('click', function(){
-        this.classList.toggle('active');
-        document.querySelector('.slide-menu__section').classList.toggle('active');
-      })
-    })
+window.addEventListener("DOMContentLoaded", function () {
+  //ハンバーガーメニューをクリックするたびにactiveクラスの付与を切り替える
+  document.querySelector(".hamburger").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".slide-menu__section").classList.toggle("active");
+  });
+});
+
+// fadeInUp
+
+$(function () {
+  $(window).scroll(function () {
+    $(".fade").each(function () {
+      var scroll = $(window).scrollTop();
+      var blockPosition = $(this).offset().top;
+      var windowHeihgt = $(window).height();
+      if (scroll > blockPosition - windowHeihgt + 100) {
+        $(this).addClass("fadeUp");
+      }
+    });
+  });
+});
+//
